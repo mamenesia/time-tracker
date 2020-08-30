fb58c25745d7e748302eca8ed6778c417ad7519d
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2020-08-30 21:52:16 +0700

    hello world

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tracker_v1/app/sign_in/sign_in_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignInPage(),
+    );
+  }
+}
